ddd060377cb6098ce4e89442237d5e0c07d981c9
OlhaNester <olga_nv@ukr.net>
2021-01-22 12:52:58 +0200

    исправления

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
 import './styles.css';
-import itemsTemplate from '../templates/gallery-items.hbs';
+import itemsTemplate from './templates/gallery-items.hbs';
 import menu from './menu.json';
 
 const markup = itemsTemplate(menu);
 const galleryRef = document.querySelector('.js-menu');
 galleryRef.insertAdjacentHTML('beforeend', markup);
+
+// сonst refs = {
+//     checkbox: document.querySelector('.theme-switch__toggle')
+
+// }
+
+// refs.checkbox.addEventListener('change', changeTheme)
+
+// function changeTheme(event) {
+
+// }
